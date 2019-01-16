749d090d499b6a0ba9ec8fe6365324cc90fb24f9
Lauren Weiss <1332094+sealauren@users.noreply.github.com>
2019-01-15 18:18:50 -0800

    trying aei params as input for 3-planet fit

--- example_planets/KOI-142/lightcurve_runscript_3pl_rvs.sh
+++ example_planets/KOI-142/lightcurve_runscript_3pl_rvs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
  
-./lcout koi142_3pl_rvs.in koi142_3pl_rvs.pldin -rv0=koi142_rvs.txt 
+# ./lcout koi142_3pl_rvs.in koi142_3pl_rvs.pldin -rv0=koi142_rvs.txt # Sean's example
+ ./lcout koi142_3pl_rvs_aei.in koi142_3pl_rvs_aei.pldin -rv0=koi142_rvs.txt # Lauren's params
 
