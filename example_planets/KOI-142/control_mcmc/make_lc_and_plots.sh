2af28bf41523e44aeac7f58b3574eeaf5c4dee72
Lauren Weiss <1332094+sealauren@users.noreply.github.com>
2019-03-25 14:50:02 -0700

    New files for coordinate system error tracking

--- example_planets/KOI-142/control_mcmc/make_lc_and_plots.sh
+++ example_planets/KOI-142/control_mcmc/make_lc_and_plots.sh
@@ -1,6 +1,6 @@
-./lightcurve_runscript_3pl_rvs.sh &
+./lightcurve_runscript_3pl_rvs.sh
 python lcplot.py
 python omc.py
-rm tbv00_03.out
-rm tbv00_02.out
+rm -f tbv00_03.out
+rm -f tbv00_02.out
 python phasefold.py
