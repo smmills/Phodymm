cc69950aade9a4ffccc821f430816e04b9cbd8a0
Lauren Weiss <1332094+sealauren@users.noreply.github.com>
2020-03-20 13:49:38 -0700

    New RV spring 2020

--- example_planets/KOI-142/control_mcmc/lightcurve_runscript_3pl_rvs.sh
+++ example_planets/KOI-142/control_mcmc/lightcurve_runscript_3pl_rvs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
  
-# ./lcout koi142_3pl_rvs.in koi142_3pl_rvs.pldin -rv0=koi142_rvs.txt # Tepoch=54.xxx, Pei Jacboian
- ./lcout koi142_3pl_rvs_xyz.in koi142_3pl_xyz.pldin -rv0=koi142_rvs.txt # Tepoch = 800.0, xyz stellocentric
+ ./lcout koi142_3pl_rvs.in koi142_3pl_rvs.pldin -rv0=koi142_rvs.txt # Tepoch=54.xxx, Pei Jacboian
+# ./lcout koi142_3pl_rvs_xyz.in koi142_3pl_xyz.pldin -rv0=koi142_rvs.txt # Tepoch = 800.0, xyz stellocentric
